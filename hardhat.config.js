a509df31efd78fd21a3877e5ce22a5ae56367d3e
piorot <rotynski@gmail.com>
2021-09-28 18:00:18 +0200

    adding pk to hh config and to gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
-
-require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
+import {Ropsten_Owner, Ropsten_Adam, Ropsten_Ewa} from './private-keys'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
   }
+
 });
+
+
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -22,22 +21,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    hardhat: {
-      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
-    },
+  solidity: "0.8.0",
+  networks:{
     ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: `https://eth-ropsten.alchemyapi.io/v2/UTzMXNfY8mfvBwQhRElRZlXYm1Uhdwce`,
+      accounts: [
+        `0x${Ropsten_Owner}`,
+        `0x${Ropsten_Adam}`,
+        `0x${Ropsten_Ewa}`,
+      ],
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  }
+  
 };
